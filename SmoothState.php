8c10d62338f51ddac7a21094561038ad9c9ac356
Dean <contact@macrowish.com>
2016-08-18 22:49:37 +0200

    Basic SmoothState Widget

---
+++ SmoothState.php
@@ -0,0 +1,38 @@
+<?php
+namespace macrowish\widgets;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+
+
+class SmoothState extends Widget
+{
+    public $options = [];
+
+    public function init()
+    {
+        $this->registerJsFile('//cdnjs.cloudflare.com/ajax/libs/smoothState.js/0.7.2/jquery.smoothState.min.js', ['depends' => [\yii\web\JqueryAsset::className()]]);
+        if (!isset($this->options['id'])) {
+            $this->options['id'] = $this->getId();
+        }
+        ob_start();
+    }
+
+    public function run()
+    {
+        $options = $this->options;
+        $div_id = $options['id'];
+        $view = $this->getView();
+
+        $js = <<<JS
+                $(function() {
+                  $('$div_id').smoothState();
+                });
+JS;
+
+        $content = ob_get_clean();
+        $response = Html::beginTag('div', ['id'=>$options['id']]) . $content . Html::endTag('div');
+        return $response;
+    }
+
+}
